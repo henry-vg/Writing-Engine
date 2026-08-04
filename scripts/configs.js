f813b5d64f2256d53d32ba05fe6e9caa9f3c3f17
henry-vg <hvg.henry@gmail.com>
2026-08-04 10:35:44 -0300

    feat: adiciona comment no contextMenu

--- scripts/configs.js
+++ scripts/configs.js
@@ -53,12 +53,6 @@ const textFileTypes = [{ description: "Text", accept: { "text/plain": [".txt", "
 const templateFileTypes = [{ description: "HTML", accept: { "text/html": [".html", ".htm"] } }];
 
 const tags = {
-    comment: {
-        values: ["c", "comment"],
-        replacement: {
-            type: "none"
-        }
-    },
     title1: {
         values: ["1", "title1", "title"],
         button: {
@@ -159,6 +153,18 @@ const tags = {
             contentLineWrapping: {
                 value: "p"
             }
+        }
+    },
+    comment: {
+        values: ["c", "comment"],
+        button: {
+            container: "contextMenu",
+            tooltip: "Comment",
+            shortcut: ";",
+            icon: "M22 4c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h14l4 4zm-2 13.17L18.83 16H4V4h16zM13 5h-2v4H7v2h4v4h2v-4h4V9h-4z"
+        },
+        replacement: {
+            type: "none"
         }
     },
     stanza: {
